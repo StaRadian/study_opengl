3e827c13abab41647181a150de544c06f08e3e37
StaRadian <47676364+StaRadian@users.noreply.github.com>
2022-01-22 22:19:11 +0900

    update #21

--- Source/src/main.cpp
+++ Source/src/main.cpp
@@ -77,6 +77,10 @@ int main(void)  //main 함수
         IndexBuffer ib(indices, 6); //IndexBuffer
 
         glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);    //정사영
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+        glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
+
+        glm::mat4 mvp = proj * view * model;
 
         Shader shader(  //Shader
             #ifdef DEBUG
@@ -88,7 +92,7 @@ int main(void)  //main 함수
             
         shader.Bind();
         //shader.SetUniform4f("u_Color",  0.8f, 0.3f, 0.8f, 1.0f);
-        shader.SetUniformMat4f("u_MVP", proj);
+        shader.SetUniformMat4f("u_MVP", mvp);
 
         Texture texture(
             #ifdef DEBUG
