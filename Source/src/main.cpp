cc66b27f7441f94061421f23c32ae2000e5ad951
StaRadian <47676364+StaRadian@users.noreply.github.com>
2022-01-24 00:55:05 +0900

    update #25

--- Source/src/main.cpp
+++ Source/src/main.cpp
@@ -12,6 +12,8 @@
 #include "Renderer.h"
 #include "Texture.h"
 
+#include "TestClearColor.h"
+
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 
@@ -68,72 +70,21 @@ int main(void)  //main 함수
     LOG(glGetString(GL_VERSION));   //OpenGL 버전 체크
     
     {
-        float positions[] = {   //x,y   //s,t
-            -50.0f,  -50.0f,  0.0f,  0.0f,      //0
-             50.0f,  -50.0f,  1.0f,  0.0f,      //1
-             50.0f,   50.0f,  1.0f,  1.0f,      //2
-            -50.0f,   50.0f,  0.0f,  1.0f       //3
-        };
-
-        unsigned int indices[] = {
-            0, 1, 2,
-            2, 3, 0
-        };
-
         GLCall(glEnable(GL_BLEND));         //Blending
         GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));  //GL_SRC_ALPHA: 0, GL_ONE_MINUS_SRC_ALPHA: 1 - 0 = 1
 
-        
-        VertexArray va; //VertexArray
-        VertexBuffer vb(positions, 4 * 4 * sizeof(float));  //VertexBuffer
-        VertexBufferLayout layout;  //VertexBufferLayout
-        layout.Push<float>(2);
-        layout.Push<float>(2);
-        va.AddBuffer(vb, layout);
-
-        IndexBuffer ib(indices, 6); //IndexBuffer
-
-        glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);    //정사영
-        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
-
-        Shader shader(  //Shader
-            #ifdef DEBUG
-                "../res/shader/Basic.shader"
-            #else
-                "./Source/res/shader/Basic.shader"
-            #endif
-            );
-            
-        shader.Bind();
-        //shader.SetUniform4f("u_Color",  0.8f, 0.3f, 0.8f, 1.0f);
-
-        Texture texture(
-            #ifdef DEBUG
-                "../res/textures/Mistarion_cat.png"
-            #else
-                "./Source/res/textures/Mistarion_cat.png"
-            #endif
-            );
-
-        texture.Bind(); //slot: 0
-        shader.SetUniform1i("u_Texture", 0);    //slot: 0
-
-        va.Unbind();
-        vb.Unbind();
-        ib.Unbind();
-        shader.Unbind();
-
         Renderer renderer;
+ 
+        test::Test* currentTest = nullptr;
+        test::TestMenu* testMenu = new test::TestMenu(currentTest);
+        currentTest = testMenu;
 
-        glm::vec3 translationA(200, 200, 0);
-        glm::vec3 translationB(400, 200, 0);
-
-        float r = 0.0f;
-        float increment = 0.05f;
+        testMenu -> RegisterTest<test::TestClearColor>("Clear Color");
 
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
         {
+            GLCall(glClearColor(0.0f, 0.0f, 0.0f, 1.0f))
             /* Render here */
             renderer.Clear();
 
@@ -142,38 +93,17 @@ int main(void)  //main 함수
             ImGui_ImplGlfw_NewFrame();
             ImGui::NewFrame();
 
+            if(currentTest)
             {
-                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
-                glm::mat4 mvp = proj * view * model;
-                shader.Bind();
-                shader.SetUniformMat4f("u_MVP", mvp);
-                
-                renderer.Draw(va, ib, shader);
-            }
-            
-            {
-                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
-                glm::mat4 mvp = proj * view * model;
-                shader.Bind();
-                shader.SetUniformMat4f("u_MVP", mvp);
-
-                renderer.Draw(va, ib, shader);
-            }
-
-            if(r > 1.0f)
-                increment = -0.05f;
-            else if(r < 0.0f)
-                increment = 0.05f;
-
-            r += increment;
-
-            {
-                ImGui::Begin("Hello, world!");
-
-                ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f);            // Edit 3 float using a slider from 0.0f to 960.0f
-                ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);            // Edit 3 float using a slider from 0.0f to 960.0f
-
-                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+                currentTest -> OnUpdate(0.0f);
+                currentTest -> OnRender();
+                ImGui::Begin("Test");
+                if (currentTest != testMenu && ImGui::Button("<-"))
+                {
+                    delete currentTest;
+                    currentTest = testMenu;
+                }
+                currentTest -> OnImGuiRender();
                 ImGui::End();
             }
 
@@ -187,7 +117,9 @@ int main(void)  //main 함수
             /* Poll for and process events */
             GLCall(glfwPollEvents());
         }
-
+        delete currentTest;
+        if(currentTest != testMenu)
+            delete testMenu;
     }
 
     // Cleanup
